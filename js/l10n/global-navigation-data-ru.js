e4a273e9ad572eab708e3f67b56a3fcceef1be9e
Robert Lihm <rlihm@lihmedia.com>
2011-10-26 12:30:40 +0200

    added current stant

---
+++ js/l10n/global-navigation-data-ru.js
@@ -0,0 +1,149 @@
+var global_navigation_data = [
+{
+  "id": "downloads",
+  "items": [
+  {
+    "link": "http://software.opensuse.org/",
+    "image": "release-stable",
+    "title": "Последний стабильный релиз",
+    "desc": "Скачать последний стабильный релиз openSUSE"
+  },
+  {
+    "link": "http://software.opensuse.org/developer/",
+    "image": "release-devel",
+    "title": "Последний релиз для разработчиков",
+    "desc": "Скачать последний релиз openSUSE для разработчиков"
+  },
+  {
+    "link": "http://software.opensuse.org/",
+    "image": "packages",
+    "title": "Поиск пакетов",
+    "desc": "Получение дополнительного программного обеспечения из openSUSE Build Service"
+  },
+  {
+    "link": "http://ru.opensuse.org/Derivatives",
+    "image": "derivatives",
+    "title": "Производные дистрибутивы",
+    "desc": "Скачайте один из специализированных дистрибутивов на основе openSUSE"
+  }
+  ]
+},
+{
+  "id": "support",
+  "items": [
+  {
+    "link": "http://ru.opensuse.org/Portal:Support",
+    "image": "help",
+    "title": "Справка",
+    "desc": "Посетите портал поддержки"
+  },
+  {
+    "link": "http://doc.opensuse.org",
+    "image": "development-documentation",
+    "title": "Документация",
+    "desc": "Читайте руководства"
+  },
+  {
+    "link": "http://ru.opensuse.org/",
+    "image": "wiki",
+    "title": "Wiki",
+    "desc": "Читайте и пишите статьи в нашей Wiki"
+  },
+  {
+    "link": "http://tube.opensuse.org",
+    "image": "video",
+    "title": "Видео",
+    "desc": "Смотрите различные руководства, презентации и записи выступлений"
+  },
+  {
+    "link": "http://forums.opensuse.org/?langid=4",
+    "image": "forums",
+    "title": "Форумы",
+    "desc": "Примите участие в обсуждениях на форуме"
+  },
+  {
+    "link": "http://ru.opensuse.org/openSUSE:Mailing_lists",
+    "image": "lists",
+    "title": "Списки рассылки",
+    "desc": "Подпишитесь на списки рассылки и читайте их архивы"
+  },
+  {
+    "link": "http://ru.opensuse.org/openSUSE:IRC_list",
+    "image": "irc",
+    "title": "IRC-каналы",
+    "desc": "Общайтесь в чатах"
+  }
+  ]
+},
+{
+  "id": "community",
+  "items": [
+  {
+    "link": "http://connect.opensuse.org/",
+    "image": "users",
+    "title": "Connect",
+    "desc": "Connect with your openSUSE friends"
+  },
+  {
+    "link": "http://planet.opensuse.org/ru/",
+    "image": "planet",
+    "title": "Planet",
+    "desc": "Читайте записи из блогов в одном потоке"
+  },
+  {
+    "link": "http://news.opensuse.org/",
+    "image": "news",
+    "title": "Новости",
+    "desc": "Последние объявления от команды"
+  },
+  {
+    "link": "http://lizards.opensuse.org/",
+    "image": "lizards",
+    "title": "Lizards",
+    "desc": "Платформа пользовательских блогов"
+  },
+  {
+    "link": "http://shop.opensuse.org/",
+    "image": "shop",
+    "title": "Магазин",
+    "desc": "Товары с символикой openSUSE!"
+  }
+  ]
+},
+{
+  "id": "development",
+  "items":
+  [
+  {
+    "link": "http://en.opensuse.org/Portal:Development",
+    "image": "developer",
+    "title": "Документация для разработчиков",
+    "desc": "Центральное место для документации по разработке"
+  },
+  {
+    "link": "http://features.opensuse.org/",
+    "image": "features",
+    "title": "Возможности",
+    "desc": "Голосуйте и разрабатывайте новые возможности openSUSE"
+  },
+  {
+    "link": "http://build.opensuse.org/",
+    "image": "buildservice",
+    "title": "Build Service",
+    "desc": "Создавайте, собирайте и улучшайте пакеты"
+  },
+  {
+    "link": "http://ru.opensuse.org/openSUSE:Submitting_bug_reports",
+    "image": "bugs",
+    "title": "Ошибки",
+    "desc": "Сообщайте о найденных ошибках"
+  },
+  {
+    "link": "http://susestudio.com",
+    "image": "studio",
+    "title": "SUSE Studio",
+    "desc": "Создавайте свои образы в Studio"
+  }
+  ]
+}
+];
