bb9e972413b076f673db9d744168abbf5d32f47e
Robert Lihm <rlihm@lihmedia.com>
2011-11-08 14:40:31 +0100

    changed view of info boxes and added JS to switch between info-boxes

--- js_local/script.js
+++ js_local/script.js
@@ -16,6 +16,30 @@ $(document).ready(function() {
     return false;
   });
   
+  // Toggle Info Columns
+  
+  $('a.info-trigger').click(function() { // open box on click
+    
+    var objID = this.id; // Get ID of clicked element
+    
+    if (objID == 'info1-trigger') { // open info-box 1
+      openInfoBox('#col-1');
+    } else if (objID == 'info2-trigger') { // open info-box 2
+      openInfoBox('#col-2');
+    };
+    
+    return false;
+  });
+
+  // Close all open info-boxes and open the requested info-box
+  function openInfoBox (id) {    
+    // close all <section> with class "active"
+    $('section.active').slideUp('fast').removeClass('active');
+
+     // open the requested info-box and add class "active"
+    $(id).slideDown('fast').addClass('active');
+  }
+  
   // Detect Browser Language
   console.log(window.navigator.language);
   
