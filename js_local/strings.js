19e3cfea880af6eed008dd8e3d4b4d3090b665f2
Klaas Freitag <freitag@suse.de>
2011-11-09 16:35:09 +0100

    localisation based on the browser lang added

---
+++ js_local/strings.js
@@ -0,0 +1,17 @@
+var i18n = new Array();
+
+i18n['de'] = {
+  'Downloads'    : 'Download',
+  'Support'      : 'Unterstützung',
+  'Community'    : 'Community',
+  'Development'  : 'Entwicklung',
+  'Sign up'      : 'Anmelden', 
+  'Login'        : 'Einloggen',
+  'Username'     : 'Benutzername',
+  'Password'     : 'Passwort',
+  'Search'       : 'Suche',
+  'openSUSE Explained': 'openSUSE erklärt',
+  'Experienced Users' : 'Erfahrene Benutzer'
+};
+
+
